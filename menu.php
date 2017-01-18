e6511d55a2dbb399fc28595057e49e05387997e9
foresitegroup <mark@foresitegrp.com>
2017-01-18 17:01:54 -0600

    2017-01-18

---
+++ menu.php
@@ -0,0 +1,38 @@
+<ul>
+  <li>
+    <a href="#">Programs</a>
+    <ul>
+      <li>Adult Programs</li>
+      <li><a href="#">Women's Fitness Gift Pack Sampling</a></li>
+      <li><a href="#">Yoga Gift Pack Sampling</a></li>
+      <li><a href="#">Yoga Instructors Gift Pack Sign Up</a></li>
+      <li><a href="#">Women's Fitness Clubs Sample Pack Sign Up</a></li>
+      <li><a href="#">Amphitheater Concert Sampling</a></li>
+      <li><a href="#">Live Theater Sampling</a></li>
+      <li><a href="#">Airline Passenger Sampling</a></li>
+      <li><a href="#">New Mother Sampling</a></li>
+      <li><a href="#">Prenatal Sampling</a></li>
+      <li class="menu-header">Student &amp; College</li>
+      <li><a href="#">Bookstore Sampling</a></li>
+      <li><a href="#">Sorority Sampling</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="#">Market Research</a>
+    <ul>
+      <li><a href="#">Payback Calculator</a></li>
+    </ul>
+  </li>
+  <li>
+    <a href="#">About</a>
+    <ul>
+      <li><a href="#">Management Profiles</a></li>
+      <li><a href="#">Our Clients</a></li>
+      <li><a href="#">Brands</a></li>
+      <li><a href="#">Synergy Sampling</a></li>
+      <li><a href="#">Distribution Partners</a></li>
+      <li><a href="#">Distribution Process</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Contact</a></li>
+</ul>
